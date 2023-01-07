42c58b014138475a51706b95b18e052bc9a8be6a
Saul Muñoz <sam.pedreros14@gmail.com>
2023-01-06 21:27:53 -0300

    Agregar switch

--- Calculadora/Calculadora.c
+++ Calculadora/Calculadora.c
@@ -22,20 +22,82 @@ int multiplicar();
 // Funcion para dividir de dos numeros
 int dividir();
 
-// Funcion para Elevar al cuadrado un numero
-int cuadrado();
-
 // Funcion para calcular el porcentaje de un numero
 int porcentaje();
+
+// Funcion para Elevar al cuadrado un numero
+int cuadrado();
 
 // Funcion para obtener raiz cuadrada de un numero
 int raiz_Cuadrada();
 
 // Funcion para  menu de opciones
-void menu_Operaciones();
+void menu_Operaciones()
+{
+    int opcion;
+    do
+    {
+        printf("Menu de Operaciones: \n");
+        printf("1. Sumar\n");
+        printf("2. Restar\n");
+        printf("3. Multiplicar\n");
+        printf("4. Dividir\n");
+        printf("5. Porcentaje\n");
+        printf("6. Cuadrado\n");
+        printf("7. Raiz Cuadrada\n");
+        printf("8. Salir\n");
+        scanf("%d", &opcion);
+
+        switch (opcion)
+        {
+        case 1:
+        {
+            sumar();
+        }
+        break;
+        case 2:
+        {
+            restar();
+        }
+        break;
+        case 3:
+        {
+            multiplicar();
+        }
+        break;
+        case 4:
+        {
+            dividir();
+        }
+        break;
+        case 5:
+        {
+            porcentaje();
+        }
+        break;
+        case 6:
+        {
+            cuadrado();
+        }
+        break;
+        case 7:
+        {
+            raiz_Cuadrada();
+        }
+        break;
+
+        default:
+        {
+            printf("Ingrese una opcion valida:");
+        }
+        break;
+        }
+
+    } while (opcion != 8);
+}
 
 int main(int argc, char const *argv[])
 {
-
+    menu_Operaciones();
     return 0;
 }
