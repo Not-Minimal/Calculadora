4d8cd5dc53558cf8df1bbed7524159f8d03b81f5
Saul Muñoz <sam.pedreros14@gmail.com>
2023-01-06 21:21:30 -0300

    Agregar funciones

--- Calculadora/Calculadora.c
+++ Calculadora/Calculadora.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-//Prototipo de funciones 
+// Prototipo de funciones
 int sumar();
 int restar();
 int multiplicar();
@@ -8,4 +8,34 @@ int dividir();
 int cuadrado();
 int porcentaje();
 int raiz_Cuadrada();
+void menu_Operaciones();
 
+// Funcion para suma de dos numeros
+int sumar();
+
+// Funcion para restar dos numeros
+int restar();
+
+// Funcion para multiplicar de dos numeros
+int multiplicar();
+
+// Funcion para dividir de dos numeros
+int dividir();
+
+// Funcion para Elevar al cuadrado un numero
+int cuadrado();
+
+// Funcion para calcular el porcentaje de un numero
+int porcentaje();
+
+// Funcion para obtener raiz cuadrada de un numero
+int raiz_Cuadrada();
+
+// Funcion para  menu de opciones
+void menu_Operaciones();
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
